87052d4e54a85e84a626e1510133855ab8e11349
Farjana Yeasmin Rimi <farjanayeasmin003@gmail.com>
2026-02-06 15:31:21 +0600

    function challenges solved

--- script.js
+++ script.js
@@ -24,3 +24,81 @@ function checkLeapYear(year) {
   }
 }
 // checkLeapYear(2100);
+
+//////////////////////////////////////////////////////////////
+
+/**
+ * function takes an array as parameter
+ * gives the average of the odd numbers in the array
+ */
+
+function oddAvg(arr) {
+  let odd = [];
+  for (number of arr) {
+    if (number % 2 === 1) {
+      odd.push(number);
+    }
+  }
+  let sum = 0;
+  for (num of odd) {
+    sum += num;
+  }
+  const avg = sum / odd.length;
+  console.log(avg);
+}
+// const numbers = [12, 3, 5, 23, 67, 8, 32];
+// oddAvg(numbers);
+
+////////////////////////////////////////////////////////////
+
+/*
+ *Remove duplicate item from an array
+ */
+
+function filtering(arr) {
+  let filterNames = [];
+  for (name of arr) {
+    if (filterNames.includes(name) === false) {
+      filterNames.push(name);
+    }
+  }
+  return filterNames;
+}
+let array = ["robita", "nobita", "kobita", "robita", "nobita", "kotha", "ruba", "robita"];
+const filterArray = filtering(array);
+// console.log(filterArray);
+
+////////////////////////////////////////////////////////////
+
+/*
+Problem 1: Word Frequency Counter
+Function name: wordFrequency(sentence)
+Statement: Count how many times each word appears in a sentence and return the result as an object.
+
+Rules:
+Ignore case differences
+Ignore extra spaces
+
+Test case 1
+Input: "I love JS and I love coding and JS is fun"
+Output: { i: 2, love: 2, js: 2, and: 2, coding: 1, is: 1, fun: 1 }
+*/
+
+function wordFrequency(sentence) {
+  let words = sentence.split(" ");
+  let count = {};
+  for (word of words) {
+    let wordLowerCase = word.toLowerCase();
+    if (count.hasOwnProperty(wordLowerCase) === true) {
+      count[wordLowerCase] += 1;
+    } else {
+      count[wordLowerCase] = 1;
+    }
+  }
+  return count;
+}
+const sentence = `I love JS and I love coding and JS js is fun Fun`;
+let result = wordFrequency(sentence);
+// console.log(result);
+
+/////////////////////////////////////////////////////////////////
